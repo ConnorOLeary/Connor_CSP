6b01c9bd3dbd18b35b9badf0ec225d62f52e6cff
ConnorOLeary <themidlvlboss@gmail.com>
2018-01-03 08:45:12 -0700

    Updated collisions

---
+++ Connor_CSP/ScreenControllers/GameControllers/DeathScene.swift
@@ -0,0 +1,36 @@
+//
+//  DeathScene.swift
+//  Connor_CSP
+//
+//  Created by Oleary, CONNOR on 12/19/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class DeathScene: SKScene
+{
+    override public func didMove(to view: SKView) -> Void
+    {
+        self.backgroundColor = SKColor.green
+        let gameOver = SKLabelNode(text: "You suck, get dead'ed")
+        gameOver.name = "game over"
+        gameOver.fontSize = 65
+        gameOver.fontColor = SKColor.red
+        gameOver.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(gameOver)
+    }
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) -> Void
+    {
+        let touch = touches.first
+        let touchLocation = touch?.location(in: self)
+        let touchedNode = self.atPoint(touchLocation!)
+        if(touchedNode.name == "game over")
+        {
+            let newGameScene = GameScene(size: size)
+            newGameScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontal
+        }
+    }
+}
